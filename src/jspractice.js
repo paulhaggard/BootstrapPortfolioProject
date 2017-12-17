bc1f6febc0985c9d4f17052a8a8e66558b41dd2b
paulhaggard <paulhaggard@gmail.com>
2017-12-16 20:22:28 -0600

    d

--- src/jspractice.js
+++ src/jspractice.js
@@ -7,9 +7,10 @@ var askName = prompt("Are you sure your name is" + hello + "?","No");
 }
 alert ("Hello, " + askName + ".  I thought it was you.");
 if (askName === "Katelyn") {
-	document.write("<div class="footer">");
+/*	document.write("<div class="footer">");
   document.write("<div class="container">");
  document.write( "<span><h3>Greetings</h3><p>Hello, Katelyn</p></span>" );
  document.write(" </div> ");
-document.write("</div>");
-}
+ document.write("</div>");*/
+alert("Hello, Katelyn.");
+ }
