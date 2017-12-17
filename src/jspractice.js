28e0ea7131ba0666c7481e35ba1f8d9f6c9d3fe1
paulhaggard <paulhaggard@gmail.com>
2017-12-16 20:28:41 -0600

    &

--- src/jspractice.js
+++ src/jspractice.js
@@ -1,9 +1,9 @@
 var hello = prompt("Hello. What is your name?");
 alert("Hi, " + hello + ".  I can see you.");
 var askName = prompt("Are you sure your name is " + hello + "?","No");
-while (askName !== "Yes" || askName !=="yes" || askName !== "y") {
-var hello = prompt("Hello. What is your name?");
-var askName = prompt("Are you sure your name is " + hello + "?","No");
+while (askName !== "Yes" && askName !=="yes" && askName !== "y") {
+var hello = prompt("Hello. Who is this?");
+var askName = prompt("Are you sure your name is " + askName + "?","No");
 }
 alert ("Hello, " + askName + ".  I thought it was you.");
 if (askName === "Katelyn") {
